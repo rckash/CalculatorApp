ed92c5c7f9db2c4e41074251d1fb02e94c8da4a0
RC <rckashyapcpe@gmail.com>
2022-11-24 22:06:28 +0800

    Button Input has been set. Next task is to fix the display.

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -10,18 +10,49 @@ import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
-    var computeDisplay: TextView = findViewById(R.id.computeDisplay)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var computeDisplay: TextView = findViewById(R.id.computeDisplay)
+        val button1: Button = findViewById(R.id.button1)
+        val button2: Button = findViewById(R.id.button2)
+        val button3: Button = findViewById(R.id.button3)
+        val button4: Button = findViewById(R.id.button4)
+        val button5: Button = findViewById(R.id.button5)
+        val button6: Button = findViewById(R.id.button6)
+        val button7: Button = findViewById(R.id.button7)
+        val button8: Button = findViewById(R.id.button8)
+        val button9: Button = findViewById(R.id.button9)
+        val buttonAdd: Button = findViewById(R.id.buttonAdd)
+        val buttonSubtract: Button = findViewById(R.id.buttonSubtract)
+        val buttonEqual: Button = findViewById(R.id.buttonEqual)
 
+        var inputDisplay: String = ""
 
-    }
-
-    fun ButtonInput (view: View) {
-        computeDisplay = computeDisplay + getText(Button)
+        button1.setOnClickListener {
+            inputDisplay = inputDisplay + "1"
+            computeDisplay.text = inputDisplay
         }
+
+        button2.setOnClickListener {
+            inputDisplay = inputDisplay + "2"
+            computeDisplay.text = inputDisplay
+        }
+
+        buttonAdd.setOnClickListener {
+            computeDisplay.text = "+"
+        }
+
+        buttonEqual.setOnClickListener {
+            computeDisplay.text = "="
+        }
+
+
     }
+
+//    fun updateText (String: compute) {
+//
+//    }
+
 }
